36ad2decaad9d64d58fddaad3bf4a79db365a9fd
jasonandjay <342690199@qq.com>
2018-06-12 16:14:40 +0800

    react-route

--- 1601N/React/league/src/router/index.jsx
+++ 1601N/React/league/src/router/index.jsx
@@ -2,83 +2,75 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Route,
-    Switch,
-    Link
+    Link,
+    NavLink,
+    Switch
 } from 'react-router-dom';
-import Info from '../components/info.jsx';
+import Com from '../components/com.jsx';
+import Login from '../components/login.jsx';
 
-const index = ({match, location})=>{
+const bg = ({match, location, history})=>{
     return <div>
-        <p>{JSON.stringify(match)}</p>
-        <p>英雄联盟信息信息简介</p>
+        {/* <p>{JSON.stringify(match)}</p> */}
+        <p>英雄联盟背景故事</p>
     </div>
 }
 
-const opera = ({match, location})=>{
+const opera = ({match, location, history})=>{
     return <div>
-        <p>{JSON.stringify(match)}</p>
-        <p>{JSON.stringify(location)}</p>
-        <p>{JSON.stringify(location.data)}</p>
-        <p>腾讯公司</p>
+        {/* <p>{JSON.stringify(match)}</p> */}
+         {/* <p>{JSON.stringify(location)}</p> */}
+        <p>英雄联盟运营商：腾讯</p>
     </div>
-}
-
-const bg = ({match, location})=>{
-    return <div>
-            <p>{JSON.stringify(match)}</p>
-            <p>{JSON.stringify(location)}</p>
-            <h1>英雄联盟</h1>
-            <h2>《英雄联盟》是由Riot Games开发及发行的一款多人在线战术
-            擂台游戏，游戏为免费模式进行并提供付费道具服务。该游戏是受到
-            《魔兽争霸III：冰封王座》中的DotA模式启发而诞生，且至今仍不断
-            定期更新</h2>
-        </div>   
 }
 
 const intro = ()=>{
     return <div>
-        <Link to="/intro/top">Top</Link>
-        <Link to="/intro/jungle">Jungle</Link>
-        <Link to="">Mid</Link>
-        <Link to="">ADCarry</Link>
-        <Link to="">Sup</Link>
+         <ul>
+            <li><NavLink to="/intro/top" activeClassName="li-active">Top</NavLink> </li>
+            <li><NavLink to="/intro/jungle">Jungle</NavLink> </li>
+            <li><NavLink to="/intro/mid">mid</NavLink> </li>
+            <li><NavLink to="/intro/ad">ADCarry</NavLink> </li>
+            <li><NavLink to="/intro/sup">Support</NavLink> </li>
+        </ul>
 
-        {/* <Route path="/intro/:info" component={heroInfo}></Route>  */}
-        <Route path="/intro/:info" component={Info}></Route>   
-    </div>
-}
-
-const heroInfo = (props)=>{
-    return <div>
-        <p>{JSON.stringify(props.match)}</p>
-        <p>{props.match.params.info}</p>
+        <Route path="/intro/:info" component={(props)=>{
+            return <p>我是{props.match.params.info}位置</p>
+        }}></Route>   
     </div>
 }
 
 export default ()=>{
     return <Router>
-        <div>
-            <Link to="/bg/1?a=1&b=2">背景故事</Link>
-            <Link to={{
-                pathname: '/opera',
-                data: {a:1,b:2}
-            }}>运营信息</Link>
-            <Link to="/intro">英雄简介</Link>
+        <div> 
+            <ul>
+                <li><Link to="/bg">背景故事</Link> </li>
+                <li><Link to={{
+                    pathname: '/opera',
+                    data: {a:1,b:2}
+                }}>运营信息</Link>  </li>
+                <li><Link to="/intro">英雄简介</Link></li>
+            </ul>
 
             <Switch>
-                <Route exact path="/" component={index}></Route>
-                <Route path="/bg/:id?" component={bg}
-                onEnter={()=>{
-                    console.log('进入路由')
-                }}
-                onLeave={()=>{
-                    console.log('离开路由')
+                <Route exact path="/" component={(props)=>{
+                    let timestamp = +new Date();
+                    // 写登录逻辑判断
+                    if (timestamp%2){
+                        // 纯组件组件的return 方式
+                        // return bg(props);
+
+                        // 常规组件的return 方式
+                        return <Com/>;
+                    }else{
+                        // return opera(props);
+                        return <Login/>;
+                    }
                 }}></Route>
+                <Route path="/bg/:id?" component={bg}></Route>
                 <Route path="/opera" component={opera}></Route>
                 <Route path="/intro" component={intro}></Route>
-            </Switch>
-           
-            {/* <Route path="/bg" component={bg}></Route> */}
+             </Switch>
         </div>
     </Router>
 }
