31fb8dcef45718f7cafcf8f1d944e99f98014da2
jason <342690199@qq.com>
2018-07-09 13:43:28 +0800

    简单实现部分生命周期

--- 1602B/React/pre_week1/src/index.js
+++ 1602B/React/pre_week1/src/index.js
@@ -2,4 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components/Index';
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+// ReactDOM.render(<Index />, document.getElementById('root'));
+
+class Demo{
+    constructor(){
+        console.log('constructor')
+    }
+
+    componentWillMount(){
+        console.log('commponentWillMount')
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')
+    }
+
+    render(){
+        return '<p>我是测试类demo</p>'
+    }
+}
+
+function render(Demo, element){
+    var demo = new Demo();
+
+    var div = document.createElement('div');
+    div.id = 'root';
+    div.innerHTML = demo.render();
+    console.log('div...', div);
+
+
+    demo.componentWillMount && demo.componentWillMount();
+    element.parentNode.replaceChild(div, element);
+    demo.componentDidMount && demo.componentDidMount();
+}
+
+render(Demo, document.getElementById('root'))
