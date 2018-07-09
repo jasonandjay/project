9f5252f7a0c898a5eb8eff367d507b821de373a8
jason <342690199@qq.com>
2018-07-09 08:22:51 +0800

    update

--- 1602B/React/pre_week1/src/components/Index.jsx
+++ 1602B/React/pre_week1/src/components/Index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {findDOMNode } from 'react-dom';
 import Dialog from './Dialog';
 
 export default class Index extends React.Component{
@@ -12,15 +13,22 @@ export default class Index extends React.Component{
     click(type){
         this.setState({
             type
+        }, ()=>{
+            console.log(findDOMNode(this.refs.dialog));
         })
+    }
+
+    componentDidMount(){
+        console.log('refs...', this.refs.alert, this.refs.dialog)
+       
     }
 
     render(){
         return <div>
-            <button onClick={()=>this.click('alert')}>打开alert提示框</button>
+            <button ref="alert" onClick={()=>this.click('alert')}>打开alert提示框</button>
             <button onClick={()=>this.click('confirm')}>打开confirm提示框</button>
             <button onClick={()=>this.click('prompt')}>打开prompt提示框</button>
-            <Dialog type={this.state.type}></Dialog>
+            <Dialog ref="dialog" type={this.state.type}></Dialog>
         </div>
     }
 }
