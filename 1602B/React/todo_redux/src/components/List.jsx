b62678e16d3061e2d1e264895d2c86205c78be5c
jason <342690199@qq.com>
2018-07-13 17:11:03 +0800

    update todo_redux

--- 1602B/React/todo_redux/src/components/List.jsx
+++ 1602B/React/todo_redux/src/components/List.jsx
@@ -8,22 +8,49 @@ class List extends React.Component{
     }
    
     render(){
-       return <div>{
+        console.log('props...',this.props);
+        return <div>
+        <p>
+            <span onClick={()=>this.props.changeType('all')}>全部</span>
+            <span onClick={()=>this.props.changeType('unfinish')}>未完成</span>
+            <span onClick={()=>this.props.changeType('finish')}>已完成</span>       
+        </p>
+        {
            this.props.list.map((item, index)=>{
-               return <li key={index}>{item}</li>
+               return <li onClick={()=>this.props.click(item.id)} style={{textDecoration:item.finish?'line-through':''}} key={index}>{item.text}</li>
            })
        }</div>
     }
 }
 const mapStateToProps = (state, ownProps)=>{
     return {
-        list: state.list
+        list: state.list.filter((item)=>{
+            if (state.type == 'finish'){
+                return item.finish;
+            }else if(state.type == 'unfinish'){
+                return !item.finish;
+            }else{
+                return true;
+            }
+        }),
+        type: state.type
     }
 }
 
 const mapDispatchToProps = (dispatch)=>{
     return {
-      
+       click: (index)=>{
+            dispatch({
+                type: 'CLICK_LIST',
+                payload: index
+            })
+       },
+       changeType: (type)=>{
+           dispatch({
+               type: 'CHANGE_TYPE',
+               payload: type
+           })
+       }
     }
 }
 
--- 1602B/React/todo_redux/src/store/reducer.js
+++ 1602B/React/todo_redux/src/store/reducer.js
@@ -1,5 +1,6 @@
 let initialState = {
     text: '123',
+    type: 'all',    //all表示全部，finish表示已完成，unfinish表示未完成
     list: []
 };
 
@@ -13,8 +14,27 @@ const changeText = (state, action)=>{
 const changeList  = (state, action)=>{
     console.log('state...', state);
     switch(action.type){
-        case 'ADD_LIST': return state.list.concat(state.text);
+        case 'ADD_LIST': return state.list.concat({
+            text: state.text,
+            finish: false,
+            id: state.list.length
+        });
+        case 'CLICK_LIST': 
+            let list = [...state.list];
+            list.forEach((item, index)=>{
+                if (item.id == action.payload){
+                    item.finish = !item.finish
+                }
+            })
+            return list;
         default: return state.list
+    }
+}
+
+const changeType = (state, action)=>{
+    switch(action.type){
+        case 'CHANGE_TYPE': return action.payload;
+        default: return state;
     }
 }
 
@@ -22,6 +42,7 @@ const changeList  = (state, action)=>{
 export default (state = initialState, action)=>{
     return {
         text: changeText(state.text, action),
-        list: changeList(state, action)
+        list: changeList(state, action),
+        type: changeType(state.type, action)
     }
 }
