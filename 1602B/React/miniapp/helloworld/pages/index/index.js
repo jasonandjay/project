be5d40fbccbc1b32a5e9cb8209000b176774e5df
jason <342690199@qq.com>
2018-07-25 08:29:05 +0800

    update

---
+++ 1602B/React/miniapp/helloworld/pages/index/index.js
@@ -0,0 +1,64 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+      imgUrls: [
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      ]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onPullDownRefresh(){
+      wx.showLoading({
+          title: '正在刷新中'
+      })
+      setTimeout(()=>{
+          wx.stopPullDownRefresh();
+          wx.hideLoading();
+      }, 5000);
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
--- 1602B/React/pagination/src/components/List.jsx
+++ 1602B/React/pagination/src/components/List.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import page from './hoc/page';
 
 class List extends React.Component{
-    constructor(){
+    constructor(props){
         super();
+        let allList = [];
+        for (let i=1;i<=500;i++){
+            allList.push(i);
+        }
+        console.log('props...', props, allList.slice(0, props.pageSize));
+        this.state= {
+            allList,
+            list: allList.slice(0, props.pageSize)
+        }
     }
 
     pageChange(page){
@@ -11,7 +20,15 @@ class List extends React.Component{
     }
 
     render(){
-        return <p>我是List</p>
+        console.log(this);
+        if (!this.state.list){
+            return null;
+        }
+        return <div>{
+            this.state.list.map((item, index)=>{
+                return <p key={index}>我是第{item}条数据</p>    
+            })
+        }</div>
     }
 }
 
