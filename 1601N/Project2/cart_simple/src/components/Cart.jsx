2d19c078736324f0bb268b45a01a24962850a597
jason <342690199@qq.com>
2018-08-14 08:56:09 +0800

    update

---
+++ 1601N/Project2/cart_simple/src/components/Cart.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import List from './List';
+
+class Cart extends Component {
+    componentDidMount(){
+        this.props.fetchList();
+    }
+
+    totalNum(){
+        let num = 0
+        this.props.list.forEach((item, index)=>{
+            if (item.isChecked){
+                num += item.num;
+            }
+        })
+        return num;
+    }
+
+    totalPrice(){
+        let price = 0
+        this.props.list.forEach((item, index)=>{
+            if (item.isChecked){
+                price += item.num*item.price;
+            }
+        })
+        return price;
+    }
+
+    render() {
+        console.log('props...', this.props);
+        return (
+            <div>
+                我是购物车
+                <List></List>
+                <p>
+                    <span>数量：{this.totalNum()}</span>
+                    <span>总价：{this.totalPrice()}</span>
+                </p>
+            </div>
+        )
+    }
+}
+
+/**
+ * 把redux中的state映射成props传递到组件中去
+ * @param {*} state 
+ * @param {*} ownProps 
+ * @return 返回需要的state
+ */
+const mapStateToPorps = (state, ownProps)=>{
+    return {
+        list: state.list
+    }
+}
+
+/**
+ * 把dispatch操作封在函数里，把函数传递到组件中直接调用
+ * @param {*} dispatch 
+ * @param {*} ownProps 
+ */
+const mapDispatchToProps = (dispatch, ownProps)=>{
+    return {
+        fetchList: ()=>{
+            fetch('/index.json')
+            .then(res=>res.json())
+            .then(body=>{
+                dispatch({
+                    type: 'FETCH_LIST',
+                    payload: body
+                })
+            })
+
+        }
+    }
+}
+
+export default connect(mapStateToPorps, mapDispatchToProps)(Cart);
---
+++ 1601N/Project2/cart_simple/src/redux/reducers.js
@@ -0,0 +1,34 @@
+// state初始值
+let initialState = {
+    list: [],           // 购物车商品列表
+    isSelectAll: false  // 购物车是否选中全部
+}
+
+// reduce处理函数
+const fetchList = (state, action)=>{
+    let list = [...state];
+    switch(action.type){
+        case 'FETCH_LIST': return action.payload;
+        case 'ITEM_CLICK': 
+            list[action.payload].isChecked = !list[action.payload].isChecked;
+            return list;
+        case 'ADD_NUM':
+            list[action.payload].num++;
+            return list;
+        case 'SUB_NUM':
+            if (list[action.payload].num != 0){
+                list[action.payload].num--;
+            }
+            return list;
+        default: return state;
+    }
+}
+    
+
+// 导出reducer
+export default (state = initialState, action)=>{
+    // 返回state
+    return {
+        list: fetchList(state.list, action)
+    }
+}
